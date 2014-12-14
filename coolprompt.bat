413b0d2742d26d32bcea475689e89fdb786ed477
8000ffff <elvin@aslanov.co>
2014-12-14 06:40:38 +0400

    files added

---
+++ coolprompt.bat
@@ -0,0 +1,9 @@
+@echo off
+::$e[1;30m[$e[1;31m$d$t$h$h$h$e[1;30m][
+::$e[1;34m%computername% %username%$e[1;30m][
+::$e[1;35m$mREMOTE$e[1;30m][
+::$e[1;36m$p$e[1;30m][
+::$e[1;33m$+PUSH$e[1;30m]
+::$_$e[1;37m$g
+::$e[1;32m
+prompt $e[1;30m[$e[1;31m$d$t$h$h$h$e[1;30m][$e[1;34m%computername% %username%$e[1;30m][$e[1;35m$m$e[1;30m][$e[1;36m$p$e[1;30m][$e[1;33m$+$e[1;30m]$_$e[1;37m$g$e[1;32m
